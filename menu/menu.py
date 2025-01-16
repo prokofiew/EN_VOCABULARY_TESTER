b4f0d154544226c3c4468170b003504dfff55273
prokofiew <filipmaciborski@gmail.com>
2025-01-16 02:04:00 +0100

    Changed comments to methods to doc-string

--- menu/menu.py
+++ menu/menu.py
@@ -25,7 +25,7 @@ class Menu(MenuInterface):
             os.system("clear")
 
     def __initialize_menu_options(self):
-        # private method to initialize Main menu
+        """ private method to initialize Main menu """
         if self.title == "Main menu":
             self.add_option(1, MenuOption(
                 "Start new test", action=self.controller.start_test))
@@ -45,11 +45,11 @@ class Menu(MenuInterface):
                 "Return to main menu", action=self.back_to_prev_menu))
 
     def add_option(self, key, option):
-        # public method to add an option to menu
+        """ Public method to add an option to menu """
         self.options[key] = option
 
     def display(self):
-        # public method to display menu
+        """ Public method to display menu """
         self.clear_console()
         print(f"\n==== {self.title} ====")
         for key, option in self.options.items():
