16363d6edbf47972dc03b2520f01ba3ca2c0c213
prokofiew <filipmaciborski@gmail.com>
2025-01-16 01:56:40 +0100

    Added comments for methods, reorganized order of code, set private methods and colorized error in category process method

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -5,9 +5,15 @@ import os
 
 from test.test import NewTest
 from menu.menu import Menu
+from colorama import Fore
 from test_managers.user_manager import UserManager
+from test_managers.text_formatter import TextFormatter
 
 TEST_DATABASE = os.path.join("test_database", "tester_database.xlsx")
+
+# class to manage the app
+# loads data when the app is started
+# responsible for execution of main menu actions
 
 
 class App_Controller:
@@ -16,9 +22,96 @@ class App_Controller:
         self.dictionaries = None
         self.vocabluary = None
         self.user_manager = UserManager()
-        self._data_load()
+        self.text_formatter = TextFormatter()
+        self.__data_load()
 
-    def _data_load(self):
+    def setup_main_menu(self):
+        """ initiating Menu """
+        self.main_menu = Menu("Main menu", self)
+
+    def start_test(self):
+        NewTest(TEST_DATABASE, self.data, self.main_menu)
+
+    def add_dictionary(self):
+        """ Adding new category of vocabulary. """
+        print("Adding a new dictionary...")
+        new_category_id = self.__process_new_category()
+        if new_category_id:
+            self.__display_new_category_info(new_category_id)
+        self.back_to_prev_menu()
+
+    def add_vocabluary(self, category_id):
+        """ Adding new vocabulary to the category,
+        setting flag if words were added"""
+        words_added = False
+        while True:
+            Menu.clear_console()
+            try:
+                new_word_EN = self.__get_user_input(
+                    "Enter new word in English: ")
+                new_word_PL = self.__get_user_input(
+                    "Enter translation in Polish: ")
+
+                new_word = pd.DataFrame({
+                    "EN": [new_word_EN],
+                    "PL": [new_word_PL],
+                    "category": [category_id],
+                })
+                self.vocabluary = self.__join_data_frames(
+                    self.vocabluary, new_word)
+                words_added = True
+
+                if input(
+                        "Do you want to add another word? (y/n): "
+                        ).lower() == "n":
+                    break
+
+            except ValueError as error:
+                message = f"An error occurred: {error}"
+                print(self.text_formatter.colorize(message, Fore.RED))
+                if input("Do you want to try again? (y/n): ").lower() == "n":
+                    break
+
+        if words_added:
+            self.__save_to_database(self.vocabluary, "vocabluary")
+        return words_added
+
+    def display_dictionaries(self):
+        """ Displays avaiable categories of vocabulary """
+        Menu.clear_console()
+        print("Available dictionaries:")
+        dictionaries_df = self.dictionaries.set_index("category_id")
+        formatted_dictionaries = [
+            f"{idx}. {category}"
+            for idx, category in enumerate(dictionaries_df["category_name"], 1)
+        ]
+        print("\n".join(formatted_dictionaries))
+        self.back_to_prev_menu()
+
+    def about_program_display(self):
+        """ Printing out description.py content """
+        Menu.clear_console()
+        print("===================================")
+        print(f"Author: {description.__author__}")
+        print(f"Version: {description.__version__}")
+        print("Description:")
+        print(description.__description__)
+        print("===================================")
+        self.back_to_prev_menu()
+
+    def back_to_prev_menu(self):
+        input("\nPress Enter to return to the previous menu...")
+
+    def exit_program(self):
+        Menu.clear_console()
+        message = "Thanks for using.\
+            You have successfully exited the program.\n"
+        print(self.text_formatter.colorize(message, Fore.CYAN))
+        sys.exit()
+
+    def __data_load(self):
+        """ Loads database file
+        and establishes sheets as properties """
         try:
             file_data = pd.ExcelFile(TEST_DATABASE)
             self.dictionaries = file_data.parse(sheet_name="categories")
@@ -32,29 +125,31 @@ class App_Controller:
             print("Database file: tester_database.xlsx not found.")
             sys.exit()
 
-    # Funkcje akcji
-    def setup_main_menu(self):
-        self.main_menu = Menu("Main menu", self)
-
-    def _validate_user_input(self, user_input):
+    def __validate_user_input(self, user_input):
+        """ Using UserManager to validate user input"""
         return UserManager.validate_user_input(user_input)
 
-    def _get_user_input(self, prompt):
+    def __get_user_input(self, prompt):
+        """ getter for user input """
         user_input = input(prompt).lower()
-        self._validate_user_input(user_input)
+        self.__validate_user_input(user_input)
         return user_input
 
-    def _join_data_frames(self, base_data_frame, added_data_frame):
+    def __join_data_frames(self, base_data_frame, added_data_frame):
+        """ Private method to join DataFrames with pd.concat """
         return pd.concat([
             base_data_frame, added_data_frame], ignore_index=True)
 
-    def _save_to_database(self, data_frame, sheet_name):
+    def __save_to_database(self, data_frame, sheet_name):
+        """ Updating database file with new vocabulary"""
         with pd.ExcelWriter(
             TEST_DATABASE, mode="a", if_sheet_exists="overlay"
         ) as writer:
             data_frame.to_excel(writer, sheet_name=sheet_name, index=False)
 
-    def _create_new_category(self, new_category):
+    def __create_new_category(self, new_category):
+        """ Creating new category_id and
+        DataFrame for new vocabulary"""
         new_category_id = int(self.dictionaries["category_id"].max() + 1)
         new_category_data_frame = pd.DataFrame(
             {
@@ -64,102 +159,37 @@ class App_Controller:
         )
         return new_category_id, new_category_data_frame
 
-    def add_vocabluary(self, category_id):
-        words_added = False
-        while True:
-            Menu.clear_console()
-            try:
-                new_word_EN = self._get_user_input(
-                    "Enter new word in English: ")
-                new_word_PL = self._get_user_input(
-                    "Enter translation in Polish: ")
-
-                new_word = pd.DataFrame({
-                    "EN": [new_word_EN],
-                    "PL": [new_word_PL],
-                    "category": [category_id],
-                })
-                self.vocabluary = self._join_data_frames(
-                    self.vocabluary, new_word)
-                words_added = True
-
-                if input(
-                        "Do you want to add another word? (y/n): "
-                        ).lower() == "n":
-                    break
-
-            except ValueError as error:
-                print(f"An error occurred: {error}")
-                if input("Do you want to try again? (y/n): ").lower() == "n":
-                    break
-
-        if words_added:
-            self._save_to_database(self.vocabluary, "vocabluary")
-        return words_added
-
-    def add_dictionary(self):
-        print("Adding a new dictionary...")
-        new_category_id = self._process_new_category()
-        if new_category_id:
-            self._display_new_category_info(new_category_id)
-        self.back_to_prev_menu()
-
-    def _process_new_category(self):
+    def __process_new_category(self):
+        """ Gets category name from user,
+        creates new category if vocabulary was added
+        """
         try:
-            new_category = self._get_user_input(
+            new_category = self.__get_user_input(
                 "\nEnter category name: ").capitalize()
             if new_category in self.dictionaries["category_name"].values:
                 raise ValueError("Category already exists.")
 
-            new_category_id, category_df = self._create_new_category(
+            new_category_id, category_df = self.__create_new_category(
                 new_category)
             if self.add_vocabluary(new_category_id):
-                self._update_dictionaries(category_df)
+                self.__update_dictionaries(category_df)
                 return new_category_id
         except ValueError as error:
-            print(f"\nError: {error}")
+            message = f"\nError: {error}"
+            print(self.text_formatter.colorize(message, Fore.RED))
         return None
 
-    def _update_dictionaries(self, new_category_df):
-        self.dictionaries = pd.concat([
-            self.dictionaries, new_category_df], ignore_index=True)
-        self._save_to_database(self.dictionaries, "categories")
-        self._data_load()
+    def __update_dictionaries(self, new_category_df):
+        """ Updates category property,
+        updates database file,
+        reloads the dabatase file """
+        self.dictionaries = self.__join_data_frames(
+            self.dictionaries, new_category_df)
+        self.__save_to_database(self.dictionaries, "categories")
+        self.__data_load()
 
-    def _display_new_category_info(self, category_id):
+    def __display_new_category_info(self, category_id):
+        """ Displaying new category DataFrame. """
         Menu.clear_console()
         print("\nNew category added successfully!")
         print(self.vocabluary[category_id == self.vocabluary["category"]])
-
-    def display_dictionaries(self):
-        Menu.clear_console()
-        print("Available dictionaries:")
-        dictionaries_df = self.dictionaries.set_index("category_id")
-        formatted_dictionaries = [
-            f"{idx}. {category}"
-            for idx, category in enumerate(dictionaries_df["category_name"], 1)
-        ]
-        print("\n".join(formatted_dictionaries))
-        self.back_to_prev_menu()
-
-    def about_program_display(self):
-        # Wyświetlanie informacji o programie
-        Menu.clear_console()
-        print("===================================")
-        print(f"Author: {description.__author__}")
-        print(f"Version: {description.__version__}")
-        print("Description:")
-        print(description.__description__)
-        print("===================================")
-        self.back_to_prev_menu()
-
-    def start_test(self):
-        NewTest(TEST_DATABASE, self.data, self.main_menu)
-
-    def back_to_prev_menu(self):
-        input("\nPress Enter to return to the previous menu...")
-
-    def exit_program(self):
-        Menu.clear_console()
-        print("Thanks for using. You have successfully exited the program.\n")
-        sys.exit()
